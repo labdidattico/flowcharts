a0893af14a96f50b68e49da4714980546a2e0bb5
andrea <michetti.andrea@gmail.com>
2026-09-23 22:25:48 +0200

    Ripristina cache service worker a 1.3.8: il commit precedente l'aveva abbassata a 1.3.1 per errore (l'app era gia' a 1.3.8)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Service worker: tiene una copia locale dell'applicazione cosi' apra anche senza rete. */
-const CACHE = 'flowcharts-v1.3.1';
+const CACHE = 'flowcharts-v1.3.8';
 const ASSETS = [
   './',
   'index.html',
